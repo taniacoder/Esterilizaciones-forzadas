f391a984e763a46cf781928f1c5ab63f27db372b
Tania Cuya Medina <tania.cuyamedina@gmail.com>
2016-12-02 13:38:53 -0500

    color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-https://codepen.io/nikname/pen/LGrOQW
 
-/**/
 $(document).ready(function(){
 	// options
 	var speed = 500; //transition speed - fade
@@ -55,45 +53,6 @@ $(document).ready(function(){
 });
 
 
-///slider2
-var slideIndex = 0;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 50000); // Change image every 2 seconds
-}
 
 ////botones traductor
 function cambia_shipibo(){
